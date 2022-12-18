f15ca802426f9b7c4c2c4cc988fe1acc1cbbb340
lapineige <lapineige@users.noreply.github.com>
2022-12-18 23:21:00 +0100

    We need python3-venv

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,7 @@
 # COMMON VARIABLES
 
 # dependencies used by the app (must be on a single line)
-#pkg_dependencies="deb1 deb2 $php_dependencies"
-# How to install python flask ?
+pkg_dependencies="python3-venv"
 
 #=================================================
 # PERSONAL HELPERS
