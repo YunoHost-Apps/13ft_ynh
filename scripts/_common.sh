34949198e415517658a3e3c04fa8c66eb059f9ac
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-01-02 14:39:48 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="python3-venv"
+#REMOVEME? pkg_dependencies="python3-venv"
 
 #=================================================
 # PERSONAL HELPERS
